4c4ea7b1097d4916c431842a9aa564d65bf843c1
Phil Leggetter <phil@leggetter.co.uk>
2023-01-08 19:49:35 +0000

    chore: format with Prettier

--- index.js
+++ index.js
@@ -1,155 +1,148 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const axios = require("axios");
 
-const RAPID_API_KEY = process.env.RAPID_API_KEY
+const RAPID_API_KEY = process.env.RAPID_API_KEY;
 
 async function checkBrand(brand) {
+  console.log("checking:", brand);
 
-    console.log('checking:', brand)
+  const results = {
+    brand: brand,
+    twitter: { available: null },
+    github: { available: null },
+    dotcom: { available: null },
+    dotio: { available: null },
+    dotco: { available: null },
+    urban_dictionary: { has_matches: null, matches: null },
+  };
 
-    const results = {
-        brand: brand,
-        twitter: {available: null},
-        github: {available: null},
-        dotcom: {available: null},
-        dotio: {available: null},
-        dotco: {available: null},
-        urban_dictionary: {has_matches: null, matches: null}
-    }
+  if (!brand) {
+    return results;
+  }
 
-    if(!brand) {
-        return results
+  try {
+    const twitter = await axios.get(`https://twitter.com/${brand}`);
+    results.twitter.available = twitter.status !== 200;
+  } catch (error) {
+    if (error.response) {
+      results.twitter.available = error.response.status === 404;
+    } else {
+      console.error(`unknown error for twitter:${brand}`);
     }
+  }
 
-    try {
-        const twitter = await axios.get(`https://twitter.com/${brand}`)
-        results.twitter.available = twitter.status !== 200
+  try {
+    const github = await axios.get(`https://github.com/${brand}`);
+    results.github.available = github.status !== 200;
+  } catch (error) {
+    if (error.response) {
+      results.github.available = error.response.status === 404;
+    } else {
+      console.error(`unknown error for twitter:${brand}`);
     }
-    catch(error) {
-        if(error.response) {
-            results.twitter.available = error.response.status === 404
-        }
-        else {
-            console.error(`unknown error for twitter:${brand}`)
-        }
-    }
+  }
 
-    try {
-        const github = await axios.get(`https://github.com/${brand}`)
-        results.github.available = github.status !== 200
-    }
-    catch(error) {
-        if(error.response) {
-            results.github.available = error.response.status === 404
-        }
-        else {
-            console.error(`unknown error for twitter:${brand}`)
-        }
-    }
+  try {
+    const dotcom = await domainr(brand, "com");
+    console.log(dotcom);
+    results.dotcom.available = dotcom.status === "undelegated inactive";
+    results.dotcom.status = dotcom.status;
+  } catch (error) {
+    console.error(error);
+    console.error("unexpected error getting .com status");
+  }
 
-    try {
-        const dotcom = await domainr(brand, 'com')
-        console.log(dotcom)
-        results.dotcom.available = dotcom.status === 'undelegated inactive'
-        results.dotcom.status = dotcom.status
-    }
-    catch(error) {
-        console.error(error)
-        console.error('unexpected error getting .com status')
-    }
+  try {
+    const dotio = await domainr(brand, "io");
+    results.dotio.available = dotio.status === "undelegated inactive";
+    results.dotio.status = dotio.status;
+  } catch (error) {
+    console.error(error);
+    console.error("unexpected error getting .io status");
+  }
 
-    try {
-        const dotio = await domainr(brand, 'io')
-        results.dotio.available = dotio.status === 'undelegated inactive'
-        results.dotio.status = dotio.status
-    }
-    catch(error) {
-        console.error(error)
-        console.error('unexpected error getting .io status')
-    }
+  try {
+    const dotco = await domainr(brand, "co");
+    results.dotco.available = dotco.status === "undelegated inactive";
+    results.dotco.status = dotco.status;
+  } catch (error) {
+    console.error(error);
+    console.error("unexpected error getting .co status");
+  }
 
-    try {
-        const dotco = await domainr(brand, 'co')
-        results.dotco.available = dotco.status === 'undelegated inactive'
-        results.dotco.status = dotco.status
-    }
-    catch(error) {
-        console.error(error)
-        console.error('unexpected error getting .co status')
-    }
+  try {
+    const urban = await axios.get(
+      `http://api.urbandictionary.com/v0/define?term=${brand}`
+    );
+    results.urban_dictionary.has_matches = urban.data.list.length > 0;
+    results.urban_dictionary.matches = urban.data.list;
+  } catch (error) {
+    console.error(`error for Urban Dictionary:${brand}`);
+    console.error(error);
+  }
 
-    try {
-        const urban = await axios.get(`http://api.urbandictionary.com/v0/define?term=${brand}`)
-        results.urban_dictionary.has_matches = urban.data.list.length > 0
-        results.urban_dictionary.matches = urban.data.list
-    }
-    catch(error) {
-        console.error(`error for Urban Dictionary:${brand}`)
-        console.error(error)
-    }
+  console.log("---------------------------");
+  console.log(JSON.stringify(results, null, 2));
 
-    console.log('---------------------------')
-    console.log( JSON.stringify(results, null, 2) )
-
-    return results
+  return results;
 }
 
 async function domainr(brand, tld) {
-    const result = await axios({
-        "method": "GET",
-        "url": "https://domainr.p.rapidapi.com/v2/status",
-        "headers": {
-            "content-type": "application/octet-stream",
-            "x-rapidapi-host": "domainr.p.rapidapi.com",
-            "x-rapidapi-key": RAPID_API_KEY
-        },
-        "params": {
-            "domain": `${brand}.${tld}`,
-            "mashape-key": RAPID_API_KEY
-        }
-    })
+  const result = await axios({
+    method: "GET",
+    url: "https://domainr.p.rapidapi.com/v2/status",
+    headers: {
+      "content-type": "application/octet-stream",
+      "x-rapidapi-host": "domainr.p.rapidapi.com",
+      "x-rapidapi-key": RAPID_API_KEY,
+    },
+    params: {
+      domain: `${brand}.${tld}`,
+      "mashape-key": RAPID_API_KEY,
+    },
+  });
 
-    return result.data.status[0]
+  return result.data.status[0];
 }
 
 const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get("/check", handleCheck)
+app.get("/check", handleCheck);
 
-app.get("/check/:brand", handleCheck)
+app.get("/check/:brand", handleCheck);
 
-async function handleCheck (request, response) {
-    console.log(request)
+async function handleCheck(request, response) {
+  console.log(request);
 
-    const brand = (request.params.brand || request.query.brand || '').trim().replace(/\s+/, '')
+  const brand = (request.params.brand || request.query.brand || "")
+    .trim()
+    .replace(/\s+/, "");
 
-    const checkResult = await checkBrand(brand)
+  const checkResult = await checkBrand(brand);
 
-    if(request.accepts('html')) {
-        response.render('pages/index', {results: checkResult});
-    }
-    else {
-        response.json(checkResult)
-    }
+  if (request.accepts("html")) {
+    response.render("pages/index", { results: checkResult });
+  } else {
+    response.json(checkResult);
+  }
 }
 
-app.get('/', (request, response) => {
-    response.json({hello: 'world'})
-})
+app.get("/", (request, response) => {
+  response.json({ hello: "world" });
+});
 
-const brand = process.argv[2]
+const brand = process.argv[2];
 if (brand) {
-    console.log('performing brand checks for', brand)
-    checkBrand(brand)
-}
-else {
-    const listener = app.listen(process.env.PORT || 3000, () => {
-        console.log("Your app is listening on port " + listener.address().port);
-    });
+  console.log("performing brand checks for", brand);
+  checkBrand(brand);
+} else {
+  const listener = app.listen(process.env.PORT || 8080, () => {
+    console.log("Your app is listening on port " + listener.address().port);
+  });
 }
